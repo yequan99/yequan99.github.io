d032ddf754348dfd91cb157864afc5f4939ff89e
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-03-07 21:57:34 +0800

    Update UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { Link } from 'react-scroll';
 
 export default function Navbar() {
     const [isDarkMode, setDarkMode] = useState(false)
@@ -11,13 +12,51 @@ export default function Navbar() {
         <>
             <div className="flex justify-between w-full h-full">
                 <div className="flex items-center pl-8 lg:pl-24 text-3xl font-bold text-pink-100">
-                    <h1>Ye Quan</h1>
+                    <Link 
+                        className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                        before:bottom-0 before:left-0 before:bg-white
+                        before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                        before:transition before:ease-in-out before:duration-300" 
+                        activeClass="active" 
+                        smooth spy to="landing">
+                            <h1>Ye Quan</h1>
+                    </Link>
                 </div>
                 <div className="flex items-center text-lg text-fuchsia-100">
                     <ul className="hidden lg:block">
-                        <li className="inline pr-24">About</li>
-                        <li className="inline pr-24">Experience</li>
-                        <li className="inline pr-24">Projects</li>
+                        <li className="inline pr-24 cursor-pointer">
+                            <Link 
+                                className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                                before:bottom-0 before:left-0 before:bg-white
+                                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                                before:transition before:ease-in-out before:duration-300" 
+                                activeClass="active" 
+                                smooth spy to="about">
+                                    About
+                            </Link>
+                        </li>
+                        <li className="inline pr-24 cursor-pointer">
+                            <Link 
+                                className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                                before:bottom-0 before:left-0 before:bg-white
+                                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                                before:transition before:ease-in-out before:duration-300" 
+                                activeClass="active"
+                                smooth spy to="experiences">
+                                    Experiences
+                            </Link>
+                        </li>
+                        <li className="inline pr-24 cursor-pointer">
+                            <Link 
+                                className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                                before:bottom-0 before:left-0 before:bg-white
+                                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                                before:transition before:ease-in-out before:duration-300" 
+                                activeClass="active" 
+                                smooth spy to="projects">
+                                    Projects
+                            </Link>
+                        </li>
                     </ul>
                     <DarkModeSwitch
                         style={{ marginRight: '2rem' }}
--- src/components/Project.js
+++ src/components/Project.js
@@ -13,16 +13,17 @@ export default function Projects() {
         {
             name: 'Recipely',
             description: (
-                <p>Recipely is a web application designed for home-cook and professional chefs. With the help of Spoonacular API, 
+                <p>
+                    Recipely is a web application designed for home-cook and professional chefs. With the help of Spoonacular API, 
                     the app aims to reduce food wastage by helping the user to keep track of remaining ingredients in their
-                    inventory. Suggested recipes based on the user's inventory are also provided by the app to give users more
-                    options.
+                    inventory.
                 </p>
             ),
-            skills: [react, nodejs]
+            skills: [react, nodejs],
+            link: ""
         },
         {
-            name: 'Sentimental Analysis on Tesla Tweets (NLP)',
+            name: 'Sentimental Analysis',
             description: (
                 <p>
                     Developed an application in Python to apply sentimental analysis and classification on Tesla Tweets using models
@@ -30,30 +31,27 @@ export default function Projects() {
                     place to sieve out relevant data.
                 </p>
             ),
-            skills: []
+            skills: [],
+            link: ""
         }
     ]
 
     return (
         <div className="w-full h-fit pl-[10%] py-10 bg-[#19192C]">
             <h1 className="text-5xl text-[#ADEFD1FF] pb-10">Projects</h1>
-            <div className="flex flex-col h-full w-full lg:flex-row lg:w-[85%]">
+            <div className="flex flex-col h-full w-[90%] lg:flex-row lg:w-[85%]">
                 {projects.map((item,index) => (
-                    <div className="pr-10 pb-10 h-full w-[95%] lg:w-[30rem] lg:pb-0">
+                    <div className="pr-10 pb-10 h-full w-full lg:w-[30rem] lg:pb-0">
                         <Card className="h-full" key={index}>
                             <CardMedia
                                 sx={{ height: 200 }}
                                 image={proj_img}
                             />
-                            <CardContent>
-                                <h1 className="text-4xl font-bold">{item.name}</h1>
+                            <CardContent sx={{ height: 280 }}>
+                                <h1 className="text-4xl font-bold text-center">{item.name}</h1>
                                 <p className="py-4">{item.description}</p>
                                 <Logo logos={item.skills} />
                             </CardContent>
-                            {/* <Button size="small">Learn More</Button> */}
-                            <CardActions>
-                                <Button size="small">Learn More</Button>
-                            </CardActions>
                         </Card>
                     </div>
                 ))}
