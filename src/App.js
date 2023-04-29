49f13d902743d221b73f9ac58a2d0ea456415912
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-04-29 16:20:36 +0800

    Rescale Landing page for phone/tablet view

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ export default function App() {
         <div className="fixed w-full top-0 z-50 h-[10%] bg-navy">
           <Navbar />
         </div>
-        <div className="fixed top-[10%] h-[90%] w-1/12 bg-navy hidden md:block">
+        <div className="fixed top-[10%] h-[90%] w-1/12 bg-navy hidden lg:block">
           <Sidebar />
         </div>
-        <div className="absolute bottom-0 right-0 h-[90%] md:float-right md:w-11/12">
+        <div className="absolute bottom-0 right-0 h-[90%] lg:float-right lg:w-11/12">
           <section id="landing" className="h-full w-full bg-navy"><Landing /></section>
-          <section id="about" className="w-full pt-12 bg-navy"><About /></section>
-          <section id="experiences" className="w-full pt-12 bg-navy"><Experiences /></section>
-          <section id="projects" className="w-full pt-12 bg-navy"><Projects /></section>
-          <div className="bg-navy md:hidden">
+          <section id="about" className="w-full pt-24 bg-navy"><About /></section>
+          <section id="experiences" className="w-full pt-24 bg-navy"><Experiences /></section>
+          <section id="projects" className="w-full pt-24 bg-navy"><Projects /></section>
+          <div className="bg-navy lg:hidden">
             <Sidebar />
           </div>
         </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -23,18 +23,18 @@ export default function Landing() {
 
     return(
         <div className="flex flex-col-reverse w-full h-full items-center lg:pb-16 lg:pl-[10%] lg:flex-row">
-            <div className="w-full text-center lg:h-fit lg:w-[45%] lg:text-left">
+            <div className="w-full text-center h-[55%] lg:h-fit lg:w-[45%] lg:text-left">
                 <span className="text-xl text-maroon" ref={el} />
                 <ul>
                     <li className="pt-10 text-6xl text-gray-200">Tan Ye Quan</li>
                     <li className="pt-10 pb-12 text-4xl text-beige">Aspiring Software Engineer</li>
                 </ul>
-                <div className="block m-auto border-2 border-white rounded-lg w-32 cursor-pointer text-center transition ease-in-out delay-350 hover:-translate-y-1 lg:m-0">
-                    <a className="text-gray-200 leading-[4rem] w-full h-full block" href={resume} target="_blank" rel="noreferrer">Resume</a>
+                <div className="m-auto border-2 border-white rounded-lg w-32 cursor-pointer text-center transition ease-in-out delay-350 hover:-translate-y-1 lg:m-0">
+                    <a className="text-gray-200 leading-[4rem] w-full h-full" href={resume} target="_blank" rel="noreferrer">Resume</a>
                 </div>
             </div>
-            <div className="w-full pb-10 lg:h-fit lg:w-[55%] lg:pb-0">
-                <img className="block m-auto h-[11rem] w-[10rem] rounded-[50%] lg:m-0 lg:h-[22rem] lg:w-[21rem]" src={profile_pic} alt="Tan Ye Quan" />
+            <div className="flex items-end pb-12 w-full h-[45%] lg:h-fit lg:w-[55%] lg:pb-0">
+                <img className="mx-auto h-[11rem] w-[10rem] rounded-[50%] lg:m-0 lg:h-[22rem] lg:w-[21rem]" src={profile_pic} alt="Tan Ye Quan" />
             </div>
         </div>
     );
